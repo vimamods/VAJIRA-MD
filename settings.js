22233273a09dffb01e24c86682716ac523c834ca
vimamods <107170132+vimamods@users.noreply.github.com>
2024-07-19 21:52:11 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> VIMAMODS zM TEAM';
 global.owner = process.env.OWNER_NUMBER  || '94776734030' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=DYcCzDhB#9dgNwDBlLfUyeRkuJBVCQDZKrzAloIvKiMabUWZ88Io' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=nVExmC4K#AmrJyVgbOqzJzEcoh6SDi5yuEaJyB3K81TSizUfdpBk' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
